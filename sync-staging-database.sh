7ca9366817f9c712b095a480c21229ee680b035a
Graham Ashton <graham@effectif.com>
2012-07-10 09:02:14 +0100

    Reset staging database before restore.
    If you rename a table in staging (e.g. when testing a migration) then
    restore the database from production the newly renamed table will be
    left in the staging db, even though it doesn't exist in production yet.
    
    Resetting the database prior to the restore will remove all tables from
    staging first.

--- sync-staging-database.sh
+++ sync-staging-database.sh
@@ -4,14 +4,32 @@ PRODUCTION_REMOTE="production"
 STAGING_REMOTE="staging"
 DATABASE_NAME="SHARED_DATABASE"
 
+## Functions
+
+reset_staging_database()
+{
+    heroku pg:reset $DATABASE_NAME -r $STAGING_REMOTE
+}
+
 latest_backup()
 {
     heroku pgbackups -r $PRODUCTION_REMOTE | tail -n 1 | cut -f 1 -d ' '
+}
+
+latest_backup_url()
+{
+    heroku pgbackups:url $(latest_backup) -r $PRODUCTION_REMOTE
+}
+
+restore_production_to_staging()
+{
+    local url="$(latest_backup_url)"
+    heroku pgbackups:restore $DATABASE_NAME "$url" -r $STAGING_REMOTE
 }
 
 ## Main program
 
 set -x
 
-URL="$(heroku pgbackups:url $(latest_backup) -r $PRODUCTION_REMOTE)"
-heroku pgbackups:restore "$DATABASE_NAME" "$URL" -r "$STAGING_REMOTE"
+reset_staging_database
+restore_production_to_staging
